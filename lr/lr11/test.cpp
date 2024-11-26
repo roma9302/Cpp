aa11fbaf617dc15ae5ff1ed9a86709b640166b8d
Roma <bshs71734@gmail.com>
2024-11-26 11:38:46 +0300

    Update test.cpp

--- lr/lr11/test.cpp
+++ lr/lr11/test.cpp
@@ -1,1 +1,23 @@
+//number 1
+#include <iostream>
+#include <string>
 
+using namespace std;
+
+int main() {
+    string words[10] = { "roman", "marom", "shrio", "qwerty", "polin", "kacha", "kappa", "nasts", "igora", "anton" };
+
+
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            if (words[i].substr(0, 3) == words[j].substr(2, 3)) {
+                swap(words[i], words[j]);
+                cout << words[j] << " и " << words[i] << " были поменяны местами " << endl;
+                break;
+            }
+
+        }
+    }
+
+    return 0;
+}
