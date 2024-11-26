2c743d1150ec2ad14d1a6c2e4944c463220e8c17
Roma <bshs71734@gmail.com>
2024-11-26 11:51:33 +0300

    Update test.cpp

--- lr/lr15/test.cpp
+++ lr/lr15/test.cpp
@@ -1,1 +1,49 @@
+//number 1
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
 
+
+using namespace std;
+
+
+int main(){
+  int* c;
+  int r;
+  cout << "input number massiv 1 " << endl;
+  cin >> r;
+  srand(time(0));
+  c = new int[r];
+  
+  
+  for(int i{}; i<r;i++){
+      c[i] = rand() %  100;
+      
+  }
+  
+  
+  for(int i {}; i<r-1;i++){
+      for(int j {};j<r-i-1;j++){
+        if(c[j]<c[j+1]){
+            int b = c[j+1];
+            c[j+1]=c[j];
+            c[j] = b;
+        }
+      }
+  }
+
+  
+  
+   for(int i{}; i<r;i++){
+        cout << i+1 << " " << c[i] << " "   << endl;
+   }
+   
+  
+  
+  
+  delete[] c;
+
+  
+  return 0;
+  }
+  
